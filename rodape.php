01e244a8a394e58bdf3a06905a3021736f443841
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2021-05-11 22:46:38 -0300

    com arquivos separados

---
+++ rodape.php
@@ -0,0 +1,8 @@
+<footer>
+&copy; Todos os direitos reservados
+</footer>
+</main>
+<script src="bootstrap5/js/bootstrap.bundle.js"></script>
+<script src="js/jquery.min.js"></script>
+</body>
+</html>
